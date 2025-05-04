cde88cd706813dab0b4581ce0ddb366a1ceff55e
Nitin Nain <nitinnain@gmail.com>
2025-05-04 15:01:28 +0530

    Added neovim base config

---
+++ nvim/README.md
@@ -0,0 +1,19 @@
+# NN's nvim config
+
+This is my customization over [nvim-lua/kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim)
+
+External Requirements:
+- Basic utils: `git`, `make`, `unzip`, C Compiler (`gcc`)
+- [ripgrep](https://github.com/BurntSushi/ripgrep#installation)
+- Clipboard tool (xclip/xsel/win32yank or other depending on the platform)
+- A [Nerd Font](https://www.nerdfonts.com/): optional, provides various icons
+  - if you have it set `vim.g.have_nerd_font` in `init.lua` to true
+- Emoji fonts (Ubuntu only, and only if you want emoji!) `sudo apt install fonts-noto-color-emoji`
+- Language Setup:
+  - If you want to write Typescript, you need `npm`
+  - If you want to write Golang, you will need `go`
+  - etc.
+
+You likely want to remove `lazy-lock.json` from your fork's `.gitignore` file
+too - it's ignored in the kickstart repo to make maintenance easier, but it's
+[recommended to track it in version control](https://lazy.folke.io/usage/lockfile).
---
+++ nvim/init.lua
@@ -0,0 +1,36 @@
+--[[
+  Built with Kickstart.nvim.
+
+  You can use `:help lua-guide` as a reference for how Neovim integrates Lua.
+    - :help lua-guide
+    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+
+  The very first thing you should do is to run the command `:Tutor` in Neovim.
+
+  Once you've completed that, you can continue working through **AND READING** the rest
+    of the kickstart init.lua.
+
+  Next, run AND READ `:help`.
+    This will open up a help window with some basic information
+    about reading, navigating and searching the builtin help documentation.
+    This should be the first place you go to look when you're stuck or confused
+    with something. It's one of my favorite Neovim features.
+
+    A keymap "<space>sh" to [s]earch the [h]elp documentation,
+    which is very useful when you're not exactly sure of what you're looking for.
+
+  If you experience any errors, run `:checkhealth` for more info.
+--]]
+
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = false
+require 'options'
+require 'keymaps'
+require 'lazy-bootstrap'
+require 'lazy-plugins'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
